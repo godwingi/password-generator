cfde91365f05f435f1142350cdd14f88431d9624
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2022-10-29 19:03:51 -0400

    inserted number and length vars, sorted variables and input validation

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment code here
 
-var specialCharacter = window.prompt(`Choose any of the following special characters: \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~ If you do not want one, leave the space blank`);
+// Variables
 
+var characters = ["\!,\",\#,\$,\%,\&,\',\(,\),\*,\+,\,,\-,\.,\/,\:,\;,\<,\=,\>,\?,\@,\[,\\,\],\^,\_,\`,\{,\|,\},\~"];
+var specialCharacter = window.prompt(`Choose any ONE of the following special characters: \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~ 
+If you do not want one, leave the space blank`);
 var lowerCase = window.prompt(`Choose a lowercase letter. "abcdefghijklmnopqrstuvwxyz" If you do not want any, leave the space blank`);
-lowerCase = lowerCase.toLowerCase();
+
 var upperCase = window.prompt(`Choose an uppercase letter. "ABCDEFGHIJKLMNOPQRSTUVWXYZ" If you do not want any, leave the space blank`);
+var numbers = window.prompt(`Choose numbers from 0-9. If you do not want any, leave the space blank`);
+var passwordLength = window.prompt(`How long would you like your password. Choose a number between 8-128. The default will be 8.`);
+
+// Input Validation
+var specialCharacter = window.prompt(`Choose any of the following special characters: \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~ If you do not want one, leave the space blank`);
+if (specialCharacter === characters){
+  console.log(passwordText);
+} else (specialCharacter !== characters);{
+  specialCharacter;
+};
+
+lowerCase = lowerCase.toLowerCase();
 upperCase = upperCase.toUpperCase();
-
-document.write(specialCharacter)
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
