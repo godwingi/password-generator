a38e76d1b0e1503caffbb009bceff8b75fb20cd1
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2022-10-30 17:49:01 -0400

    password prompt and button function

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 // Assignment code here
+// Prompt elements
+
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", function(){
+  var desiredLength = prompt("Choose character between 8-128:");
+  var lowerCaseLetters = confirm("Include lowercase characters?\nEither OK or Cancel.");
+  var upperCaseLetters = confirm("Include uppercase characters?\nEither OK or Cancel.");
+  var numerical = confirm("Include numbers?\nEither OK or Cancel.");
+  var symbols = confirm("Include special characters?\nEither OK or Cancel.");
+
+function generatePassword(){
+  var length = +desiredLength.value;
+  var yesLower =  lowerCaseLetters === true;
+  var yesUpper = upperCaseLetters === true;
+  var yesNumbers = numerical === true;
+  var yesSymbols = symbols === true;
+
+  passwordText = generatePassword(yesLower, yesUpper, yesNumbers,yesSymbols, length);
+}
+}
+);
+const randomizer = {
+  lower: lowerCase,
+  upper: upperCase,
+  number: numbers,
+  character: specialCharacters,
+}; 
+
+
 // Generator fucntions
 function lowerCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -18,9 +53,8 @@ function specialCharacters() {
 }
 
 
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-// // Write password to the #password input
+
+// // // Write password to the #password input
 // function writePassword() {
 //   var password = generatePassword();
 //   var passwordText = document.querySelector("#password");
