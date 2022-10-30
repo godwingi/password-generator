45c72b07fcb4f0950b16fa34a0182326e4930578
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2022-10-29 21:51:34 -0400

    special character validation loop

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 
 // Variables
 
-var characters = ["\!,\",\#,\$,\%,\&,\',\(,\),\*,\+,\,,\-,\.,\/,\:,\;,\<,\=,\>,\?,\@,\[,\\,\],\^,\_,\`,\{,\|,\},\~"];
-var specialCharacter = window.prompt(`Choose any ONE of the following special characters: \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~ 
-If you do not want one, leave the space blank`);
+var characters = ["!","#", "$", "%", "&", "'","(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var specialCharacter = window.prompt(`Choose any ONE of the following special characters: \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~`);
 var lowerCase = window.prompt(`Choose a lowercase letter. "abcdefghijklmnopqrstuvwxyz" If you do not want any, leave the space blank`);
 
 var upperCase = window.prompt(`Choose an uppercase letter. "ABCDEFGHIJKLMNOPQRSTUVWXYZ" If you do not want any, leave the space blank`);
@@ -12,13 +11,16 @@ var numbers = window.prompt(`Choose numbers from 0-9. If you do not want any, le
 var passwordLength = window.prompt(`How long would you like your password. Choose a number between 8-128. The default will be 8.`);
 
 // Input Validation
-var specialCharacter = window.prompt(`Choose any of the following special characters: \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~ If you do not want one, leave the space blank`);
-if (specialCharacter === characters){
+function checkCharacter(){
+if (specialCharacter == characters){
   console.log(passwordText);
 } else (specialCharacter !== characters);{
-  specialCharacter;
+  window.prompt(`Choose any ONE of the following special characters: \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~ 
+  If you do not require one, leave the space blank`)
+  return;
 };
-
+}
+specialCharacter = checkCharacter();
 lowerCase = lowerCase.toLowerCase();
 upperCase = upperCase.toUpperCase();
 
